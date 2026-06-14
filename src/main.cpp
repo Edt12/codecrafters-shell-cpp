0583a7b97e68dcb1d070e19182cd0bd0306c32c4
ed <edtickle@icloud.com>
2026-06-14 22:16:33 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <system_error>
 
 int main() {
   // Flush after every std::cout / std:cerr
+  // 
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  try{
+
+      std::string input;
+      // TODO: Uncomment the code below to pass the first stage
+      std::cout << "$ ";
+      std::cin >> input;
+      if(true){
+          throw std::runtime_error(input + ": command not found");
+      }
+    
+     
+      
+  }catch(const std::exception &ex){
+      std::cerr <<ex.what();
+  }
+
+  return 0;
 }
